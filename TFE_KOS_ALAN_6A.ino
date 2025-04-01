ac280adb02021c55613f33cc2e2600bcbfcb2f65
AlanKos <Alan.kos@inraci.be>
2025-04-01 19:26:42 +0200

    coments
    je veux dormir

--- TFE_KOS_ALAN_6A.ino
+++ TFE_KOS_ALAN_6A.ino
@@ -2,6 +2,13 @@
 // Kos Alan 6A
 // Station de mesure embarquée pour voiture
 // Utilise : ESP32 DEVKIT V1, MPU_9250, GPSPA1010D, BMP280
+//****************COMENTAIRES*******************
+//aucune idee comment le string marche jdois voir
+//faut optimiser!
+//bordel en loop
+//plein de trucs en double jpense faut corriger
+//FAUT FAIRE FONCTIONNER LE LORA!!!!
+//SD comment sa marche ???
 //**********************************************
 #include "fonctions.h"
 void setup() {
